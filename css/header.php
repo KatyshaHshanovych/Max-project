7c63e9dd72d4d180bdbb617626cd7dd99c7d1d83
Kateryna Hshanovych <katrina.90994@gmail.com>
2022-01-27 20:29:02 +0200

    create header block and info block

---
+++ css/header.php
@@ -0,0 +1,32 @@
+<div class="block-header">
+    <div class="block-header__navMenu">
+        <ul class="block-header__linksItem">
+            <li class="block-header__links">
+                <a href="/" class="block-header__link">
+                    Home
+                </a>
+            </li>
+            <li class="block-header__links">
+                <a href="/" class="block-header__link">
+                    Project
+                </a>
+            </li>
+            <li class="block-header__links">
+                <a href="/" class="block-header__link">
+                    Portfolio
+                </a>
+            </li>
+            <li class="block-header__links">
+                <a href="/" class="block-header__link">
+                    Contact
+                </a>
+            </li>
+            <li class="block-header__links">
+                <a href="/" class="block-header__link">
+                    Feedback
+                </a>
+            </li>
+        </ul>
+    </div>
+    <hr class="block-header__line">
+</div>
--- index.php
+++ index.php
@@ -1,13 +1,38 @@
 <!doctype html>
-<html lang="en">
+<html lang="ru">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+    <title>Portfolio</title>
 </head>
 <body>
-
+    <?php include 'css/header.php'; ?>
+    <div class="block-background">
+        <div class="container">
+            <div class="block-background__info">
+                <div class="block-background__leftInfo">
+                    <div class="block-background__title">
+                        Cherevatyi <br>
+                        Maksym
+                    </div>
+                    <hr class="block-background__underLine">
+                    <div class="block-background__profession">
+                        Character Animator
+                    </div>
+                    <div class="block-background__buttonDownload">
+                        <a href="/" class="block-background__setting">
+                            Download CV
+                        </a>
+                        <div class="block-background__iconBlank">
+                            <img src="/image/icon/pdf.svg" alt="pdf" title="pdf">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
