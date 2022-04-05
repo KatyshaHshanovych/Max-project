6683fe7e9c832dacd7a7e636cc60629b3f2739b8
Kateryna Hshanovych <katrina.90994@gmail.com>
2022-04-05 16:20:11 +0300

    finished the first block and connect fonts

--- index.php
+++ index.php
@@ -13,21 +13,50 @@
     <div class="block-background">
         <div class="container">
             <div class="block-background__info">
-                <div class="block-background__leftInfo">
-                    <div class="block-background__title">
-                        Cherevatyi <br>
-                        Maksym
+                <div class="block-background__general">
+                    <div class="block-background__leftInfo">
+                        <div class="block-background__title">
+                            Cherevatyi <br>
+                            Maksym
+                        </div>
+                        <hr class="block-background__underLine">
+                        <div class="block-background__profession">
+                            Character Animator
+                        </div>
+                        <div class="block-background__buttonDownload">
+                            <a href="/" class="block-background__setting">
+                                Download CV
+                            </a>
+                            <div class="block-background__iconBlank">
+                                <img src="/image/icon/pdf.svg" alt="pdf" title="pdf" class="block-background__imgIcon">
+                            </div>
+                        </div>
                     </div>
-                    <hr class="block-background__underLine">
-                    <div class="block-background__profession">
-                        Character Animator
-                    </div>
-                    <div class="block-background__buttonDownload">
-                        <a href="/" class="block-background__setting">
-                            Download CV
-                        </a>
-                        <div class="block-background__iconBlank">
-                            <img src="/image/icon/pdf.svg" alt="pdf" title="pdf">
+                    <div class="block-background__rightInfo">
+                        <div class="block-background__mailPortfolio">
+                            <p class="block-background__textMail">cherewatyi.maksim@gmail.com
+                            </p>
+                        </div>
+                        <div class="block-background__email">
+                            <a href="cherewatyi.maksim@gmail.com">
+                            </a>
+                            <div class="block-background__iconContact">
+                                <a href="cherewatyi.maksim@gmail.com">
+                                    <img src="/image/icon/Subtract.png" alt="google" title="google">
+                                </a>
+                                <a href="cherewatyi.maksim@gmail.com">
+                                    <img src="/image/icon/facebook.png" alt="facebook" title="facebook">
+                                </a>
+                                <a href="cherewatyi.maksim@gmail.com">
+                                    <img src="/image/icon/vimeo.png" alt="vimeo" title="vimeo">
+                                </a>
+                                <a href="cherewatyi.maksim@gmail.com">
+                                    <img src="/image/icon/linkedin.png" alt="linkedin" title="linkedin">
+                                </a>
+                                <a href="cherewatyi.maksim@gmail.com">
+                                    <img src="/image/icon/twitter.png" alt="twitter" title="twitter">
+                                </a>
+                            </div>
                         </div>
                     </div>
                 </div>
