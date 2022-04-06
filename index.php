d8579248501c092c9978a6bcb06b36bb53f6cd9a
Kateryna Hshanovych <katrina.90994@gmail.com>
2022-04-06 20:23:01 +0300

    create a new block Project and slider

--- index.php
+++ index.php
@@ -6,6 +6,11 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Anek+Bangla:wght@300;400;500;600&display=swap" rel="stylesheet">
+    <link href="owl-carousel/owl.carousel.min.css">
+    <link href="owl-carousel/owl.theme.default.min.css">
     <title>Portfolio</title>
 </head>
 <body>
@@ -63,5 +68,83 @@
             </div>
         </div>
     </div>
+    <div class="block-AboutMe">
+        <div class="container">
+            <div class="block-AboutMe__title">
+                about me
+            </div>
+            <hr class="block-AboutMe__lineText">
+            <div class="block-AboutMe__animator">
+                <div class="block-AboutMe__leftImage">
+                    <img src="image/animator.png" alt="animator" title="animator">
+                </div>
+                <div class="block-AboutMe__infoAnimator">
+                    <div class="block-AboutMe__blockInfo">
+                        <div class="block-AboutMe__name">
+                            Cherevatyi Maksym
+                        </div>
+                        <div class="block-AboutMe__character">
+                            Character animator
+                        </div>
+                        <div class="block-AboutMe__hrLine">
+                            <hr class="block-AboutMe__line">
+                        </div>
+                        <div class="block-AboutMe__summary">
+                            Summary
+                        </div>
+                        <div class="block-AboutMe__textBlock">
+                            Highly motivated and inspired Animation graduate <br>
+                            looking to fill a position as a Character Animator. <br>
+                            I am a dedicated and focused team player, seeking <br>
+                            an opportunity to work with other animators and directors <br> to produced high quality animation assets
+                            <br> according to the company’s style and goals.
+                        </div>
+                        <div class="block-AboutMe__href">
+                            <a href="/" class="block-AboutMe__color">
+                                Industry Experience
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+        <div class="block-Project">
+            <div class="container">
+                <div class="block-Project__head">
+                    Project
+                </div>
+                <hr class="block-AboutMe__lineText">
+                <div class="block-Project__itemSlider">
+                    <div class="block-Project__selected">
+                        Selected Projects
+                    </div>
+                    <div class="block-Project__allProject">
+                        <a href="/" class="block-Project__item">
+                            See all
+                        </a>
+                        <div class="block-Project__iconSlider">
+                            <div class="block-Project__imgArrow">
+                                <img class="block-Project__imageOne" src="image/arrow.png" alt="arrow" title="arrow">
+                            </div>
+                            <div class="block-Project__imgEllipse">
+                                <img class="block-Project__image" src="image/Ellipse.png" alt="ellipse" title="ellipse">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="owl-carousel">
+                    <div> Your Content </div>
+                    <div> Your Content </div>
+                    <div> Your Content </div>
+                    <div> Your Content </div>
+                    <div> Your Content </div>
+                    <div> Your Content </div>
+                    <div> Your Content </div>
+                </div>
+            </div>
+        </div>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/jquery.min.js"></script>
 </body>
 </html>
